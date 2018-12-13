5bed55ffb93763546ad088da1db1e3ba4e665038
curious <365739387@qq.com>
2018-12-13 16:04:56 +0800

    for py3 only

--- jaccard.py
+++ jaccard.py
@@ -5,14 +5,8 @@ import re
 # 自然语言处理包
 import jieba
 import jieba.analyse
-# 转义字符实体化的包,py2-3兼容
-try:
-    # for py3
-    import html
-except:
-    # for py2
-    import HTMLParser
-    html = HTMLParser.HTMLParser()
+# 转义字符实体化的包
+import html
 
 
 def extract_keywords(content):
@@ -42,12 +36,12 @@ def jaccard(x,y):
     # 除零处理
     similarity = float(intersection)/float(union) if union != 0 else 0
     # 返回值 （1.相似度 2.第一个内容有意义的词的个数 3.第二个内容有意义的词的个数
-    return similarity,len(keywords_x),len(keywords_y)
+    return similarity
 
 
 # 测试
 if __name__ == '__main__':
     content_one = '<span style="font-size: 16px; font-family: arial, helvetica, sans-serif;">2,&nbsp;Elastic cuffs and stretch fabric provide snug-fitting fte dwadw ddawd and games.</span>'
     content_two = '<span style="font-size: 16px; font-family: arial, helvetica, sans-serif;">2,&nbsp;Elastic cuffs and stretch fabric provide snug-fitting fte dwadw ddawd and test.</span>'
-    similarity, len_one, len_two = jaccard(content_one, content_two)
-    print(similarity,len_one,len_two)
+    similarity = jaccard(content_one, content_two)
+    print('相似度: %.2f%%'% (similarity*100)) 
